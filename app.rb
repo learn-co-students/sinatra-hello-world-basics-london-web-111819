3bd2b2e23bdc187d5ccb90b531a372705e959674
github username <github email address>
2019-12-09 15:38:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  get '/' do
+    "Hello, World!"
+  end
 
-
+  # 1. Write a `get` method in the `app.rb` file to respond to the root URL '/'.
+  # 2. The `get` route should respond with the string `"Hello, World!"`.
 end
